94a3160b449c3b4a9e5ce5ebafa8b59d82304ff0
Ramil Aliev <raliev@hiconversion.ru>
2018-10-25 20:31:41 +0300

    Typescript configuration

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,4 @@
 import { configure } from 'enzyme';
-import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new ReactSixteenAdapter() });
+configure({ adapter: new Adapter() });
