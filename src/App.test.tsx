7de7badaf66f1468b580342d3ab6491d2332d815
Ramil Aliev <raliev@hiconversion.ru>
2018-10-25 17:33:07 +0300

    Set up tests + basic layout

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
+import { shallow } from 'enzyme';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Smoke', () => {
+  it('App renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
---
+++ src/Clock/Clock.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class Clock extends React.Component {
+  public render() {
+    return <div> i am clock! </div>;
+  }
+}
+
+export default Clock;
