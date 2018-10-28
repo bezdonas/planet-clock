5adbee6bcf6446d5826a586ae1d8bcfcd63fa8ca
Ramil Aliev <raliev@hiconversion.ru>
2018-10-28 23:25:00 +0300

    Refactored interfaces

--- src/Clock/Clock.tsx
+++ src/Clock/Clock.tsx
@@ -18,50 +18,9 @@ import {
   StyledMinute,
   StyledSecond,
 } from './ClockStyles';
+import { IClock } from 'src/types/Clock';
 
-// TODO: refactor interfaces for functions
-export const getRotateAnimation = (from: number, to: number): Keyframes => {
-  return keyframes`
-    from {
-      transform: rotate(${from}deg);
-    }
-    to {
-      transform: rotate(${to}deg);
-    }
-  `;
-};
-
-export const positionArrowComponent = (
-  styledArrow: StyledComponentClass<{}, {}>,
-  position: number
-): StyledComponentClass<{}, {}> => {
-  return styled(styledArrow)`
-    transform: rotate(${position}deg);
-  `;
-};
-
-interface AnimateArrowComponentArgs {
-  positionedArrow: StyledComponentClass<{}, {}>;
-  rotateAnimation: Keyframes;
-  duration: number;
-  steps?: number;
-}
-export const animateArrowComponent = (
-  settings: AnimateArrowComponentArgs
-): StyledComponentClass<{}, {}> => {
-  const { positionedArrow, rotateAnimation, duration, steps } = settings;
-  const stepsString = steps ? `steps(${steps})` : '';
-  return styled(positionedArrow)`
-    animation: ${rotateAnimation} ${duration}ms ${stepsString} infinite;
-  `;
-};
-
-interface GetDurationsResult {
-  minute: number;
-  hour: number;
-  day: number;
-}
-export const getDurations = (secondDuration: number): GetDurationsResult => {
+export const getDurations = (secondDuration: number): IClock.Durations => {
   const minuteDuration: number = secondDuration * 60;
   const hourDuration: number = minuteDuration * 60;
   const dayDuration: number = hourDuration * 12;
@@ -72,16 +31,11 @@ export const getDurations = (secondDuration: number): GetDurationsResult => {
   };
 };
 
-interface TimeToDegreesResult {
-  second: number;
-  minute: number;
-  hour: number;
-}
 export const timeToDegrees = (
   hour: number,
   minute: number,
   second: number
-): TimeToDegreesResult => {
+): IClock.Time => {
   return {
     second: Math.round(degsPerSec * second),
     minute: Math.round(degsPerMin * (minute + second / secsPerCircle)),
@@ -89,58 +43,97 @@ export const timeToDegrees = (
   };
 };
 
-export interface ClockProps {
+const getRotateAnimation = (from: number, to: number): Keyframes => {
+  return keyframes`
+    from {
+      transform: rotate(${from}deg);
+    }
+    to {
+      transform: rotate(${to}deg);
+    }
+  `;
+};
+
+const positionArrowComponent = (
+  styledArrow: StyledComponentClass<{}, {}>,
+  position: number
+): StyledComponentClass<{}, {}> =>
+  styled(styledArrow)`
+    transform: rotate(${position}deg);
+  `;
+
+const animateArrowComponent = (
+  settings: IClock.AnimateArrowComponentArgs
+): StyledComponentClass<{}, {}> => {
+  const { positionedArrow, rotateAnimation, duration, steps } = settings;
+  const stepsSetting = steps ? `steps(${steps})` : '';
+  return styled(positionedArrow)`
+    animation: ${rotateAnimation} ${duration}ms ${stepsSetting} infinite;
+  `;
+};
+
+const generateAnimatedArrows = (
+  startingPositions: IClock.Time,
+  durations: IClock.Durations
+): IClock.AnimatedArrows => {
+  const AnimatedStyledSecond = animateArrowComponent({
+    positionedArrow: positionArrowComponent(
+      StyledSecond,
+      startingPositions.second
+    ),
+    rotateAnimation: getRotateAnimation(
+      startingPositions.second,
+      startingPositions.second + degsPerCircle
+    ),
+    duration: durations.minute,
+    steps: 60,
+  });
+  AnimatedStyledSecond.displayName = 'AnimatedStyledSecond'; // for tests
+
+  const AnimatedStyledMinute = animateArrowComponent({
+    positionedArrow: positionArrowComponent(
+      StyledMinute,
+      startingPositions.minute
+    ),
+    rotateAnimation: getRotateAnimation(
+      startingPositions.minute,
+      startingPositions.minute + degsPerCircle
+    ),
+    duration: durations.hour,
+  });
+  AnimatedStyledMinute.displayName = 'AnimatedStyledMinute'; // for tests
+
+  const AnimatedStyledHour = animateArrowComponent({
+    positionedArrow: positionArrowComponent(StyledHour, startingPositions.hour),
+    rotateAnimation: getRotateAnimation(
+      startingPositions.hour,
+      startingPositions.hour + degsPerCircle
+    ),
+    duration: durations.day,
+  });
+  AnimatedStyledHour.displayName = 'AnimatedStyledHour'; // for tests
+
+  return {
+    AnimatedStyledSecond,
+    AnimatedStyledMinute,
+    AnimatedStyledHour,
+  };
+};
+
+export type ClockProps = {
   secondDuration: number;
-  hour: number;
-  minute: number;
-  second: number;
-}
+} & IClock.Time;
 
 export default class Clock extends React.PureComponent<ClockProps> {
   public render() {
     const { hour, minute, second, secondDuration } = this.props;
     const durations = getDurations(secondDuration);
     const startingPositions = timeToDegrees(hour, minute, second);
-
-    const AnimatedStyledSecond = animateArrowComponent({
-      positionedArrow: positionArrowComponent(
-        StyledSecond,
-        startingPositions.second
-      ),
-      rotateAnimation: getRotateAnimation(
-        startingPositions.second,
-        startingPositions.second + degsPerCircle
-      ),
-      duration: durations.minute,
-      steps: secsPerCircle,
-    });
-    AnimatedStyledSecond.displayName = 'AnimatedStyledSecond';
-
-    const AnimatedStyledMinute = animateArrowComponent({
-      positionedArrow: positionArrowComponent(
-        StyledMinute,
-        startingPositions.minute
-      ),
-      rotateAnimation: getRotateAnimation(
-        startingPositions.minute,
-        startingPositions.minute + degsPerCircle
-      ),
-      duration: durations.hour,
-    });
-    AnimatedStyledMinute.displayName = 'AnimatedStyledMinute';
-
-    const AnimatedStyledHour = animateArrowComponent({
-      positionedArrow: positionArrowComponent(
-        StyledHour,
-        startingPositions.hour
-      ),
-      rotateAnimation: getRotateAnimation(
-        startingPositions.hour,
-        startingPositions.hour + degsPerCircle
-      ),
-      duration: durations.day,
-    });
-    AnimatedStyledHour.displayName = 'AnimatedStyledHour';
+    const {
+      AnimatedStyledSecond,
+      AnimatedStyledMinute,
+      AnimatedStyledHour,
+    } = generateAnimatedArrows(startingPositions, durations);
 
     return (
       <StyledClockWrapper>
---
+++ src/types/Clock.d.ts
@@ -0,0 +1,28 @@
+import styled, { Keyframes, StyledComponentClass } from 'styled-components';
+
+export namespace IClock {
+  export interface Time {
+    hour: number;
+    minute: number;
+    second: number;
+  }
+
+  export interface Durations {
+    minute: number;
+    hour: number;
+    day: number;
+  }
+
+  export interface AnimateArrowComponentArgs {
+    positionedArrow: StyledComponentClass<{}, {}>;
+    rotateAnimation: Keyframes;
+    duration: number;
+    steps?: number;
+  }
+
+  export interface AnimatedArrows {
+    AnimatedStyledSecond: StyledComponentClass<{}, {}>;
+    AnimatedStyledMinute: StyledComponentClass<{}, {}>;
+    AnimatedStyledHour: StyledComponentClass<{}, {}>;
+  }
+}
