38b2d8fdfd0972c5e773fdf5cb1b3e833cc4b3f7
Ramil Aliev <bezdonas@gmail.com>
2018-10-29 20:28:09 +0300

    Added more test cases for arrows, removed auto-generated animation-names from animation rule checks

--- src/Clock/Arrows/__tests__/HourArrow.spec.tsx
+++ src/Clock/Arrows/__tests__/HourArrow.spec.tsx
@@ -3,16 +3,31 @@ import React from 'react';
 import 'jest-styled-components';
 import HourArrow from '../HourArrow';
 
-it('has expected style rule', () => {
-  const props = {
-    position: 176,
-    circleDuration: 30000,
-  };
-  const wrapper = mount(<HourArrow {...props} />);
-  const AnimatedStyledHour = wrapper.find('AnimatedStyledHour');
-  expect(AnimatedStyledHour).toHaveStyleRule('transform', 'rotate(176deg)');
-  expect(AnimatedStyledHour).toHaveStyleRule(
-    'animation',
-    'cCwVsE 30000ms infinite'
-  );
+describe('Check passed styles', () => {
+  it('has expected style rule 1', () => {
+    const props = {
+      position: 176,
+      circleDuration: 30000,
+    };
+    const wrapper = mount(<HourArrow {...props} />);
+    const AnimatedStyledHour = wrapper.find('AnimatedStyledHour');
+    expect(AnimatedStyledHour).toHaveStyleRule('transform', 'rotate(176deg)');
+    expect(AnimatedStyledHour).toHaveStyleRule(
+      'animation',
+      expect.stringContaining('30000ms infinite')
+    );
+  });
+  it('has expected style rule 2', () => {
+    const props = {
+      position: 132,
+      circleDuration: 214444,
+    };
+    const wrapper = mount(<HourArrow {...props} />);
+    const AnimatedStyledHour = wrapper.find('AnimatedStyledHour');
+    expect(AnimatedStyledHour).toHaveStyleRule('transform', 'rotate(132deg)');
+    expect(AnimatedStyledHour).toHaveStyleRule(
+      'animation',
+      expect.stringContaining('214444ms infinite')
+    );
+  });
 });
--- src/Clock/Arrows/__tests__/MinuteArrow.spec.tsx
+++ src/Clock/Arrows/__tests__/MinuteArrow.spec.tsx
@@ -3,16 +3,31 @@ import React from 'react';
 import 'jest-styled-components';
 import MinuteArrow from '../MinuteArrow';
 
-it('has expected style rule', () => {
-  const props = {
-    position: 42,
-    circleDuration: 50000,
-  };
-  const wrapper = mount(<MinuteArrow {...props} />);
-  const AnimatedStyledMinute = wrapper.find('AnimatedStyledMinute');
-  expect(AnimatedStyledMinute).toHaveStyleRule('transform', 'rotate(42deg)');
-  expect(AnimatedStyledMinute).toHaveStyleRule(
-    'animation',
-    'jNoktf 50000ms infinite'
-  );
+describe('Check passed styles', () => {
+  it('has expected style rule 1', () => {
+    const props = {
+      position: 42,
+      circleDuration: 50000,
+    };
+    const wrapper = mount(<MinuteArrow {...props} />);
+    const AnimatedStyledMinute = wrapper.find('AnimatedStyledMinute');
+    expect(AnimatedStyledMinute).toHaveStyleRule('transform', 'rotate(42deg)');
+    expect(AnimatedStyledMinute).toHaveStyleRule(
+      'animation',
+      expect.stringContaining('50000ms infinite')
+    );
+  });
+  it('has expected style rule 2', () => {
+    const props = {
+      position: 13,
+      circleDuration: 753210,
+    };
+    const wrapper = mount(<MinuteArrow {...props} />);
+    const AnimatedStyledMinute = wrapper.find('AnimatedStyledMinute');
+    expect(AnimatedStyledMinute).toHaveStyleRule('transform', 'rotate(13deg)');
+    expect(AnimatedStyledMinute).toHaveStyleRule(
+      'animation',
+      expect.stringContaining('753210ms infinite')
+    );
+  });
 });
